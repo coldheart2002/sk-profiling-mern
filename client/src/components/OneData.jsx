560ded3862e8d6a6bd673a157274c4974268809c
coldHeart <tampipininomharlito@gmail.com>
2024-03-02 21:25:46 +0800

    test

---
+++ client/src/components/OneData.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const OneData = ({ data }) => {
+  return (
+    <>
+      <h1>
+        Last Name: <span>{data.lName}</span>
+      </h1>
+    </>
+  );
+};
+
+export default OneData;
